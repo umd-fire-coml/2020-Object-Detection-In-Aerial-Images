a48a4fdaf07a228df3298f188f1a116dfd5268f7
Pranay Kuppa <pranay.kuppa@gmail.com>
2020-10-01 18:17:42 -0400

    Adding regular bounding boxes

--- data_download.py
+++ data_download.py
@@ -7,6 +7,9 @@ from zipfile import ZipFile
 if not(os.path.exists('data/train')):
     os.makedirs('data/train')
 
+if not(os.path.exists('data/train/annotations_hbb/')):
+    os.makedirs('data/train/annotations_hbb/')
+
 if not(os.path.exists('data/train/annotations/')):
     os.makedirs('data/train/annotations/')
 
@@ -15,6 +18,9 @@ if not(os.path.exists('data/test')):
 
 if not(os.path.exists('data/validation')):
     os.makedirs('data/validation')
+
+if not(os.path.exists('data/validation/annotations_hbb')):
+    os.makedirs('data/validation/annotations_hbb')
 
 if not(os.path.exists('data/validation/annotations')):
     os.makedirs('data/validation/annotations')
@@ -25,10 +31,11 @@ if not(os.path.exists('data/validation/annotations')):
 urls = ['https://drive.google.com/uc?export=download&id=1zb_kEOXTtpsMuWAtuTtMIPLVUSpjGPPw',
 'https://drive.google.com/uc?export=download&id=1JBWCHdyZOd9ULX0ng5C9haAt3FMPXa3v', 
 'https://drive.google.com/uc?export=download&id=1pEmwJtugIWhiwgBqOtplNUtTG2T454zn',
-'https://drive.google.com/uc?export=download&id=1-vLCMhIW9CV2cmCPPBbDR9_hdecf5bLb']
+'https://drive.google.com/uc?export=download&id=1-vLCMhIW9CV2cmCPPBbDR9_hdecf5bLb',
+'https://drive.google.com/uc?export=download&id=12uPWoADKggo9HGaqGh2qOmcXXn-zKjeX']
 
 outputs = ['data/train/part1.zip', 'data/train/part2.zip', 'data/train/part3.zip', 
-'data/train/annotations/annotations.zip']
+'data/train/annotations_hbb/annotations.zip', 'data/train/annotations/annotations.zip']
 
 for i in range(len(urls)):
     gdown.download(urls[i],outputs[i], quiet = False)
@@ -37,9 +44,11 @@ for i in range(len(urls)):
 
 # %% validation
 urls = ['https://drive.google.com/uc?export=download&id=1uCCCFhFQOJLfjBpcL5MC0DHJ9lgOaXWP',
-'https://drive.google.com/uc?export=download&id=1XDWNx3FkH9layL8jVUkEHJ_-CY8K4zse']
+'https://drive.google.com/uc?export=download&id=1XDWNx3FkH9layL8jVUkEHJ_-CY8K4zse',
+'https://drive.google.com/uc?export=download&id=1FkCSOCy4ieNg1UZj1-Irfw6-Jgqa37cC']
 
-outputs = ['data/validation/validation.zip', 'data/validation/annotations/annotations.zip']
+outputs = ['data/validation/validation.zip', 'data/validation/annotations_hbb/annotations.zip',
+'data/validation/annotations/annotations.zip']
 
 for i in range(len(urls)):
     gdown.download(urls[i],outputs[i], quiet = False)
@@ -59,13 +68,14 @@ for i in range(len(urls)):
 # %% Unzipping
 
 zip_paths = ['data/train/part1.zip', 'data/train/part2.zip', 'data/train/part3.zip',
-'data/train/annotations/annotations.zip', 'data/valdation/validation.zip', 
-'data/validation/annotations/annotations.zip', 'data/test/part1.zip', 'data/test/part2.zip']
+'data/train/annotations_hbb/annotations.zip', 'data/valdation/validation.zip', 
+'data/validation/annotations_hbb/annotations.zip', 'data/test/part1.zip', 'data/test/part2.zip',
+'data/train/annotations/annotations.zip','data/validation/annotations/annotations.zip']
 
-outputs = ['data/train/', 'data/train/', 'data/train/', 'data/train/annotations/',
-'data/validation/', 'data/validation/annotations/', 'data/test/', 'data/test/']
+outputs = ['data/train/', 'data/train/', 'data/train/', 'data/train/annotations_hbb/',
+'data/validation/', 'data/validation/annotations_hbb/', 'data/test/', 'data/test/','data/train/annotations/',
+'data/validation/annotations/']
 
 for i in range(len(zip_paths)):
     with ZipFile(zip_paths[i], 'r') as zip_ref:
         zip_ref.extractall(outputs[i])
-        
