a4902c09fbfa26642fd624c015038672bb4b7fdf
SnoodTood <wooalec@gmail.com>
2020-11-03 17:16:30 -0500

    Found an IoU function and added basic demonstration

---
+++ accuracy.py
@@ -0,0 +1,12 @@
+from keras import backend as K
+
+# Pilfered shamelessly from towardsdatascience.com:
+# https://towardsdatascience.com/metrics-to-evaluate-your-semantic-segmentation-model-6bcb99639aa2
+
+# y_true and y_pred are intended to be m by r by c by n matrices representing the number of images, rows, columns, and classes
+
+def iou_coef(y_true, y_pred, smooth=1):
+  intersection = K.sum(K.abs(y_true * y_pred), axis=[1,2,3])
+  union = K.sum(y_true,[1,2,3])+K.sum(y_pred,[1,2,3])-intersection
+  iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
+  return iou
