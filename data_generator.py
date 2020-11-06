a84db7d49edf6208bfcd6ae5d18ea1f9a7e8c238
Viktor Murray <viktro78@gmail.com>
2020-11-06 18:16:58 -0500

    Don't recreate generator part 2

--- data_generator.py
+++ data_generator.py
@@ -63,6 +63,8 @@ class DOTASequence(Sequence):
                             skiprows=2,
                             converters={8: lambda s: labels_to_idx[s.decode("utf-8")]},
                         ).astype(np.uint16)
+                        self.annotations[img_name].shape = (-1, 10)
+                        self.annotations[img_name][:, :8] -= 1
                         break
 
     def __len__(self):
@@ -99,74 +101,74 @@ class SegmentationSequence(DOTASequence):
         augmenter=None,
         batch_size=5,
         output_img_size=(480, 480),
+        steps_per_epoch=1000,
     ):
         super().__init__(img_path, annot_path, augmenter, batch_size)
         self.output_img_size = output_img_size
+        self.steps_per_epoch = steps_per_epoch
+
+        self.generator = self.batch_generator()
+
+    def __len__(self):
+        return self.steps_per_epoch
+
+    def batch_generator(self):
+        start_pos = (0, 0)
+        while True:
+            for img_name in self.image_names:
+                img = cv2.imread(os.path.join(self.img_path, img_name + ".png"))
+                bbox_set = self.annotations[img_name]
+
+                if self.augmenter:
+                    img, bbox_set = self.augmenter(img, bbox_set)
+
+                mask = np.zeros(
+                    (img.shape[0], img.shape[1], len(labels_to_idx)), dtype=np.uint8
+                )
+                for bbox in bbox_set:
+                    try:
+                        box_class = bbox[8]
+                    except IndexError as e:
+                        print(img_name)
+                        print(bbox)
+                        raise e
+
+                    bbox = bbox[:8].reshape((4, 2))
+
+                    left = np.min(bbox, axis=0)
+                    right = np.max(bbox, axis=0)
+                    x = np.arange(left[0], right[0] + 1)
+                    y = np.arange(left[1], right[1] + 1)
+                    xv, yv = np.meshgrid(x, y, indexing="xy")
+                    points = np.hstack((xv.reshape((-1, 1)), yv.reshape((-1, 1))))
+
+                    path = matplotlib.path.Path(bbox)
+                    bbox_mask = path.contains_points(points)
+                    bbox_mask.shape = xv.shape
+                    try:
+                        mask[
+                            left[1] : right[1] + 1,
+                            left[0] : right[0] + 1,
+                            box_class,
+                        ] += bbox_mask
+                    except ValueError as e:
+                        pass
+
+                for x in range(start_pos[0], img.shape[1], self.output_img_size[0]):
+                    for y in range(start_pos[1], img.shape[0], self.output_img_size[1]):
+                        yield img[
+                            y : min(y + self.output_img_size[1], img.shape[0]),
+                            x : min(x + self.output_img_size[0], img.shape[1]),
+                        ], mask[
+                            y : min(y + self.output_img_size[1], mask.shape[0]),
+                            x : min(x + self.output_img_size[0], mask.shape[1]),
+                        ]
 
     def __getitem__(self, idx):
-        def batch_gen():
-            start_pos = (0, 0)
-            while True:
-                for img_name in self.image_names:
-                    img = cv2.imread(os.path.join(self.img_path, img_name + ".png"))
-                    bbox_set = self.annotations[img_name]
+        imgs, masks = zip(*[next(self.generator) for i in range(self.batch_size)])
 
-                    if self.augmenter:
-                        img, bbox_set = self.augmenter(img, bbox_set)
-
-                    mask = np.zeros(
-                        (img.shape[0], img.shape[1], len(labels_to_idx)), dtype=np.uint8
-                    )
-                    for bbox in bbox_set:
-                        box_class = bbox[8]
-                        bbox = bbox[:8].reshape((4, 2))
-
-                        left = np.min(bbox, axis=0)
-                        right = np.max(bbox, axis=0)
-                        x = np.arange(left[0], right[0] + 1)
-                        y = np.arange(left[1], right[1] + 1)
-                        xv, yv = np.meshgrid(x, y, indexing="xy")
-                        points = np.hstack((xv.reshape((-1, 1)), yv.reshape((-1, 1))))
-
-                        path = matplotlib.path.Path(bbox)
-                        bbox_mask = path.contains_points(points)
-                        bbox_mask.shape = xv.shape
-                        try:
-                            mask[
-                                left[1] : right[1] + 1,
-                                left[0] : right[0] + 1,
-                                box_class,
-                            ] += bbox_mask
-                        except ValueError as e:
-                            print(bbox)
-                            print(mask.shape)
-                            print(img_name)
-                            print(
-                                mask[
-                                    left[1] : right[1] + 1,
-                                    left[0] : right[0] + 1,
-                                    box_class,
-                                ].shape
-                            )
-                            print(left)
-                            print(right)
-                            print(str(left[0]) + ", " + str(right[0]))
-                            raise e
-
-                    for x in range(start_pos[0], img.shape[1], self.output_img_size[0]):
-                        for y in range(
-                            start_pos[1], img.shape[0], self.output_img_size[1]
-                        ):
-                            yield img[
-                                y : min(y + self.output_img_size[1], img.shape[0]),
-                                x : min(x + self.output_img_size[0], img.shape[1]),
-                            ], mask[
-                                y : min(y + self.output_img_size[1], mask.shape[0]),
-                                x : min(x + self.output_img_size[0], mask.shape[1]),
-                            ]
-
-        generator = batch_gen()
-        imgs, masks = zip(*[next(generator) for i in range(self.batch_size)])
+        imgs = list(imgs)
+        masks = list(masks)
 
         for i, img in enumerate(imgs):
             if img.shape != (self.output_img_size[1], self.output_img_size[0], 3):
