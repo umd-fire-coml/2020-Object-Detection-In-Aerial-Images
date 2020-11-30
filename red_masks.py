abe6ed7a5bae536465478dc5ea9632fcfbd3785d
pkuppa <pranay.kuppa@gmail.com>
2020-11-29 19:59:09 -0500

    Update red_masks.py
    Got rid of test cell

--- red_masks.py
+++ red_masks.py
@@ -1,15 +1,6 @@
 # %%
 import os
 import cv2
-# %% test cell
-#flag = os.path.exists('data/train/masks/')
-#image = cv2.imread('data/train/masks/P0001_instance_color_RGB.png')
-#
-#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#cv2.imshow('test', image)
-#cv2.imwrite('red_example.png', image)
-#cv2.waitKey()
-
 
 #%%
 directory = 'data/train/masks/'
